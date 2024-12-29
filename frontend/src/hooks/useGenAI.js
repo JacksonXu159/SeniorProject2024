48dddd7272039b3049655d8d00e5f821caaa0f27
cyw37 <cyw37@drexel.edu>
2024-12-29 17:22:44 -0500

    updated project structure

--- frontend/src/hooks/useGenAI.js
+++ frontend/src/hooks/useGenAI.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import OpenAI from "openai";
 
-const useGenAI = () => {
-  return (
-    <div>useGenAI</div>
-  )
+const OPENAI_API_KEY = import.meta.env.VITE_OPENAI_API_KEY
+
+const useGenAI = async (content) => {
+    const openai = new OpenAI({
+        apiKey: OPENAI_API_KEY
+    });
+    
+    const completion = await openai.chat.completions.create({
+        model: "gpt-4o-mini",
+        messages: [
+            { role: "system", content: "You are a financial assistant" },
+            {
+                role: "user",
+                content: "content",
+            },
+        ],
+    });
+    
+    console.log(completion.choices[0].message);
+    return completion.choices[0].message
 }
+
+useGenAI("Hi, what is your role?")
 
 export default useGenAI
