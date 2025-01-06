0a88e998a336402b1d16de6f3ab4be5e08f63b86
cyw37 <cyw37@drexel.edu>
2025-01-06 18:34:36 -0500

    Pages created and routed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,9 @@ import theme from "./utils/theme";
 import { Routes, Route } from "react-router-dom";
 
 import Dashboard from "./pages/Dashboard";
+import Account from "./pages/Account"
+import Services from "./pages/Services"
+import Settings from "./pages/Settings"
 import Navbar from "./components/Navbar";
 import ChatPopUp from "./components/ChatPopUp";
 
@@ -15,6 +18,9 @@ function App() {
 
         <Routes>
           <Route path="/" element={<Dashboard />} />
+          <Route path="/account" element={<Account/>} />
+          <Route path="/services" element={<Services/>} />
+          <Route path="/settings" element={<Settings/>} />
         </Routes>
         
         <ChatPopUp />
