9be9733529cdf49a99259de258baf6902b093b5a
cyw37 <cyw37@drexel.edu>
2025-01-16 17:36:59 -0500

    Lang chain set up

--- backend/chatbot_langchain.py
+++ backend/chatbot_langchain.py
@@ -1,18 +1,55 @@
 import os
 from langchain.agents import Tool
+from langchain_openai import ChatOpenAI
+from langchain.agents import (
+    create_openai_functions_agent,
+    Tool,
+    AgentExecutor,
+)
+from langchain.prompts import (
+    PromptTemplate,
+    SystemMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+    ChatPromptTemplate,
+)
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 
-# CHATBOT_AGENT_MODEL = os.getenv("CHATBOT_AGENT_MODEL")
+from agents.assistant import assistant_chain
 
-# chat_model = ChatOpenAI(
-#     model=HOSPITAL_AGENT_MODEL,
-#     temperature=0,
-# )
+CHATBOT_AGENT_MODEL = os.getenv("CHATBOT_AGENT_MODEL")
 
-# tools = [
-#     Tool(
-#         name="Consultant"
-#     )
-# ]
+tools = [
+    Tool(
+        name="Consultant",
+        func=assistant_chain.invoke,
+        description="""Useful when you need to answer general finance related questions. 
+        not useful for any data or user specific questions such as account balance, statements,
+        user's investments and etc. Pass the entire question as input to the tool. For intance,
+        if the question is "What are stocks and how they work?", the input should be "What are stocks and how they work?"
+        """
+    )
+]
 
-# if __name__ == "__main__":
-#     pass
+chatbot_agent_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are a helpful chatbot assistant"),
+        MessagesPlaceholder("chat_history", optional=True),
+        ("human", "{input}"),
+        MessagesPlaceholder("agent_scratchpad"),
+    ]
+)
+
+chat_model = ChatOpenAI(model="gpt-4o-mini", temperature=0)
+
+chatbot_agent = create_openai_functions_agent(
+    llm=chat_model,
+    prompt=chatbot_agent_prompt,
+    tools=tools,
+)
+
+chatbot_agent_executor = AgentExecutor(
+    agent=chatbot_agent,
+    tools=tools,
+    return_intermediate_steps=True,
+    verbose=True,
+)
