4ad8b5c18510dbb6fd45b6aeb6ff3b8982d6b24a
cyw37 <cyw37@drexel.edu>
2025-01-16 16:59:57 -0500

    Basic chatbot assistant created

---
+++ backend/chatbot_langchain.py
@@ -0,0 +1,18 @@
+import os
+from langchain.agents import Tool
+
+# CHATBOT_AGENT_MODEL = os.getenv("CHATBOT_AGENT_MODEL")
+
+# chat_model = ChatOpenAI(
+#     model=HOSPITAL_AGENT_MODEL,
+#     temperature=0,
+# )
+
+# tools = [
+#     Tool(
+#         name="Consultant"
+#     )
+# ]
+
+# if __name__ == "__main__":
+#     pass
