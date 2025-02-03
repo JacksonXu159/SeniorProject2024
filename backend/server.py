b33f0e126c31577a393b16e8648b3911aefa4bee
Charles Wu <110290661+ywcharles@users.noreply.github.com>
2025-02-03 16:20:22 -0500

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -19,7 +19,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  
+    allow_origins=["http://localhost:5173", "http://10.0.0.79:5173/"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
