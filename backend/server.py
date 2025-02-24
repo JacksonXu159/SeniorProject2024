d601e7e5f6bf502d137098ec6637db4c9ff1e68a
dangkhoa <ddn38@drexel.edu>
2025-02-24 14:37:20 -0500

    Access-Control-Allow-Origin server fix

--- backend/server.py
+++ backend/server.py
@@ -19,7 +19,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  
+    allow_origins=["http://18.208.173.29"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
