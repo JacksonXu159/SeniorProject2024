bb56ca7ff341b9cd2c4cfc3b993222e7ddcaf709
cyw37 <cyw37@drexel.edu>
2025-03-13 23:23:06 -0400

    use config url

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:8000' // Local
-// const API_URL = 'http://44.193.233.90:8000' // Prod
+// const API_URL = 'http://localhost:8000' // Local
+const API_URL = 'http://44.193.233.90:8000' // Prod
 
 export { API_URL};
